c19f641b6eeda1d11c0de279cba0ec577ccb9546
DanielBuenoNavarroo <danbue419@alu.centroafuera.es>
2024-02-13 12:35:01 +0100

    Metido firebase falta codigo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
+    // google firebase
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -47,4 +50,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
 }
