36d546407545e5ac6d82682e6d9105dd6d713dda
Sebastian-II-El-Suspendido <sebyan396@alu.centroafuera.es>
2024-03-06 00:32:56 +0100

    recycler final

--- app/src/main/java/com/example/proyectofinalpmm/CiudadActivity.kt
+++ app/src/main/java/com/example/proyectofinalpmm/CiudadActivity.kt
@@ -3,9 +3,12 @@ package com.example.proyectofinalpmm
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.HorizontalScrollView
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.proyectofinalpmm.databinding.ActivityCiudadBinding
@@ -18,15 +21,9 @@ class CiudadActivity : BaseActivity() {
         setContentView(binding.root)
         val btnEntrar = findViewById<Button>(R.id.btnEntrar)
         val btnContinuarCiudad = findViewById<Button>(R.id.btnContinuarCiudad)
-        binding.ScrollComprar.visibility = View.GONE
-        binding.btnEntrar2.visibility= View.GONE
-
-        val globalButton = findViewById<ImageView>(R.id.ajustesBoton)
-        globalButton.setOnClickListener {
-            val intentb = Intent(this,AjustesActivity::class.java)
-            startActivity(intentb)
-        }
-
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerCity)
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.layoutManager = layoutManager
         val ciudadesList = listOf(
             R.drawable.casa1,
             R.drawable.casa2,
@@ -41,7 +38,6 @@ class CiudadActivity : BaseActivity() {
             R.drawable.casa11,
             R.drawable.casa12,
         )
-
         val zonasList = listOf(
             "Distrito de las Mil Fuentes",
             "Barrio de los Sueños Encantados",
@@ -56,14 +52,22 @@ class CiudadActivity : BaseActivity() {
             "Sector de las Mariposas de Cristal",
             "Área de la Aurora Brillante"
         )
-
-        val recyclerView: RecyclerView = findViewById(R.id.recyclerCity)
-        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        recyclerView.layoutManager = layoutManager
-
-
         val adapter = ImageAdapter(this, ciudadesList, zonasList)
         recyclerView.adapter = adapter
+
+        val scrollComprar = findViewById<LinearLayout>(R.id.ScrollComprar)
+        scrollComprar.visibility = View.GONE
+        binding.btnEntrar2.visibility= View.GONE
+
+        val globalButton = findViewById<ImageView>(R.id.ajustesBoton)
+        globalButton.setOnClickListener {
+            val intentb = Intent(this,AjustesActivity::class.java)
+            startActivity(intentb)
+        }
+
+
+        Log.v("Casas",ciudadesList.size.toString())
+        Log.v("Barrios",zonasList.size.toString())
 
         animateText(binding.textView,"Entras a la ciudad (Espero que con los bolsillos bien cargados de oro) con la necesidad de adquirir una nueva propiedad donde poder descansar de tus aventuras. Se te presentan distintas propiedas que puedes comprar")
 
@@ -74,14 +78,14 @@ class CiudadActivity : BaseActivity() {
 
         btnContinuarCiudad.setOnClickListener {
             binding.textView.visibility=View.GONE
-            binding.ScrollComprar.visibility=View.VISIBLE
+            scrollComprar.visibility=View.VISIBLE
             binding.btnEntrar.visibility=View.GONE
             binding.btnContinuarCiudad.visibility=View.GONE
         }
 
         binding.btnEntrar2.setOnClickListener {
             binding.textView.visibility=View.VISIBLE
-            binding.ScrollComprar.visibility=View.GONE
+            scrollComprar.visibility=View.GONE
             binding.btnEntrar.visibility=View.VISIBLE
             binding.btnContinuarCiudad.visibility=View.VISIBLE
             binding.btnEntrar2.visibility=View.GONE
